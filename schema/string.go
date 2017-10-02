a252027f8b10ba27a310b4be7f574ac7f3949032
hwchiu <hwchiu@linkernetworks.com>
2017-10-02 16:00:26 +0800

    add constraint function for String type

--- schema/string.go
+++ schema/string.go
@@ -2,10 +2,10 @@ package schema
 
 import (
 	"fmt"
+	"github.com/satori/go.uuid"
 	"net/mail"
 	"net/url"
-
-	"github.com/satori/go.uuid"
+	"regexp"
 )
 
 // Valid string formats and configuration.
@@ -17,6 +17,55 @@ const (
 	stringUUIDVersion = 4
 )
 
+func checkStringConstraints(v, pattern string, min, max int, t string) (string, error) {
+	fmt.Println(pattern)
+	fmt.Println(min, len(v))
+	fmt.Println(max)
+	if min != 0 && len(v) <= min {
+		return v, fmt.Errorf("constraint check error: %s:%v < minimum:%v", t, v, min)
+	}
+
+	if max != 0 && len(v) >= max {
+		return v, fmt.Errorf("constraint check error: %s:%v > maximum:%v", t, v, max)
+	}
+
+	if pattern != "" {
+		match, err := regexp.MatchString(pattern, v)
+		if false == match || err != nil {
+			return v, fmt.Errorf("constraint check error: %s:%v don't fit pattern : %v ", t, v, pattern)
+		}
+	}
+
+	return v, nil
+}
+
+func decodeString(format, value string, c Constraints) (string, error) {
+
+	_, err := checkStringConstraints(value, c.Pattern, c.MinLength, c.MaxLength, StringType)
+
+	if err != nil {
+		return value, err
+	}
+
+	switch format {
+	case stringURI:
+		_, err := url.ParseRequestURI(value)
+		return value, err
+	case stringEmail:
+		_, err := mail.ParseAddress(value)
+		return value, err
+	case stringUUID:
+		v, err := uuid.FromString(value)
+		if v.Version() != stringUUIDVersion {
+			return value, fmt.Errorf("invalid UUID version - got:%d want:%d", v.Version(), stringUUIDVersion)
+		}
+		return value, err
+	}
+	// NOTE: Returning the value for unknown format is in par with the python library.
+
+	return value, nil
+
+}
 func castString(format, value string) (string, error) {
 	switch format {
 	case stringURI:
